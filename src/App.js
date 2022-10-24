df5c8a1e0a5c26c8822d346662d95507b0116fa4
git <jj@jjs-MacBook-Pro.local>
2022-10-24 17:34:52 -0400

    counter api call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [hits, setCount] = useState({ data: [] });
+
+  const handleClick = async () => {
+
+    const response = await fetch('https://api.countapi.xyz/hit/1ccb732e-b55a-4404-ad3f-0f99c02fe44e', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const result = await response.json();
+
+    console.log('result is: ', JSON.stringify(result));
+    alert(JSON.stringify(result))
+
+
+  }
+
+
+
   return (
+
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Project Purple Cow
-        </p>
+        <h1>
+          <span>Project</span> Purple Cow
+        </h1>
+        <button onClick={handleClick} className="btn btn-primary">
+          Click me
+        </button>
+
       </header>
     </div>
+
   );
 }
 
